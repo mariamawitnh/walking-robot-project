bd04c46f37291560a1d03d3fb45563f326fa8878
marie <marie.kjelbergnes@gmail.com>
2026-04-11 13:17:25 +0200

    simplified some things

--- marie_tmp/walkingrobot.py
+++ marie_tmp/walkingrobot.py
@@ -8,19 +8,31 @@ from roboticstoolbox.backends.PyPlot import PyPlot
 
 
 class WalkingRobot:
-    def __init__(self, goal_list: list, dt_anim=0.01, cycle_time=4.01, anim_skip_every=3):
+    """ A class for reusing the robot things, units in metres (usually) """
+    GOAL_RADIUS = 0.005  # 5mm
+    BODY_WIDTH = 0.100
+    BODY_LENGTH = 0.200
+    L1 = L2 = 0.200
+
+    def __init__(self,
+                 goal_list: list,
+                 dt_anim=0.01,
+                 cycle_time=4.01,
+                 anim_skip_every=3
+                 ):
         """ For some reason, ``cycle_time`` needs to be a float. It can never be an int """
-        gait_dt = 0.01
-        step_size = max(1, round(dt_anim / gait_dt))
+        self._dt = dt_anim
+        self._gait_dt = 0.01
+        self._step_size = max(1, round(dt_anim / self._gait_dt))
+
+        self.leg = self._create_leg()
+        self.legs = self._create_legs(self.leg)
+        self.leg_offsets = self._create_leg_offsets()
+
+        self._skip = anim_skip_every
         """ Init the class itself, prepare everything to run later """
         print("Init...")
         mm = 0.001
-        L1 = 100 * mm
-        L2 = 100 * mm
-
-        print("creating the leg")
-        leg = rt.ERobot(rt.ET.Rz() * rt.ET.Rx() * rt.ET.ty(L1) * rt.ET.Rx() * rt.ET.tz(-L2))
-        print("Leg: ", leg)
 
         xf = 50
         xb = -xf
@@ -37,12 +49,12 @@ class WalkingRobot:
         ]) * mm
 
         print("create trajectory\n")
-        x = rt.mstraj(segments, tsegment=[3, 0.25, 0.5, 0.25], dt=gait_dt, tacc=0.07)
+        x = rt.mstraj(segments, tsegment=[3, 0.25, 0.5, 0.25], dt=self._gait_dt, tacc=0.07)
 
         print("inverse kinematics (this will take a moment)....", end='')
         xcycle = x.q
         xcycle = np.vstack((xcycle, xcycle[-3:, :]))
-        sol = leg.ikine_LM(sm.SE3(xcycle), mask=[1, 1, 1, 0, 0, 0])
+        sol = self.leg.ikine_LM(sm.SE3(xcycle), mask=[1, 1, 1, 0, 0, 0])
         print("done")
         qcycle = sol.q
 
@@ -52,20 +64,6 @@ class WalkingRobot:
 
         W = 100 * mm
         L = 200 * mm
-
-        leg_offsets = [
-            sm.SE3(L/2,  -W/2, 0),
-            sm.SE3(-L/2,  -W/2, 0),
-            sm.SE3(L/2,   W/2, 0) * sm.SE3.Rz(np.pi),
-            sm.SE3(-L/2,   W/2, 0) * sm.SE3.Rz(np.pi),
-        ]
-
-        legs = [
-            rt.ERobot(leg, name='leg0'),
-            rt.ERobot(leg, name='leg1'),
-            rt.ERobot(leg, name='leg2'),
-            rt.ERobot(leg, name='leg3')
-        ]
 
         n_steps = 10000  # deprecated idk
         total_arc = body_vel * dt_anim * n_steps  # approx 2.0 m
@@ -82,8 +80,8 @@ class WalkingRobot:
 
         T_wb = sm.SE3(0, 0, 0)
 
-        for i, leg_robot in enumerate(legs):
-            leg_robot.base = T_wb * leg_offsets[i]
+        for i, leg_robot in enumerate(self.legs):
+            leg_robot.base = T_wb * self.leg_offsets[i]
             leg_robot.q = np.r_[0, 0, 0]
             env.add(leg_robot, readonly=True, jointaxes=False, eeframe=False, shadow=False)
 
@@ -111,7 +109,7 @@ class WalkingRobot:
             heading_error = (bearing - theta + np.pi) % (2*np.pi) - np.pi
             turn_rad_per_step = K_p * heading_error * dt_anim
             dist_to_goal = np.hypot(goal[0] - pos_x, goal[1] - pos_y)
-            if dist_to_goal < 0.005:   # within 5 mm
+            if dist_to_goal < self.GOAL_RADIUS:
                 print(f"Goal {goal} reached at step {i}!")
                 i_goal += 1
                 print(f"Next goal is {goal_list[i_goal]}")
@@ -121,10 +119,10 @@ class WalkingRobot:
                 break
 
             # 1. Update joint angles
-            legs[0].q = self._gait(qcycle, i * step_size,   0, False)
-            legs[1].q = self._gait(qcycle, i * step_size, 100, False)
-            legs[2].q = self._gait(qcycle, i * step_size, 200, True)
-            legs[3].q = self._gait(qcycle, i * step_size, 300, True)
+            self.legs[0].q = self._gait(qcycle, i * self._step_size,   0, False)
+            self.legs[1].q = self._gait(qcycle, i * self._step_size, 100, False)
+            self.legs[2].q = self._gait(qcycle, i * self._step_size, 200, True)
+            self.legs[3].q = self._gait(qcycle, i * self._step_size, 300, True)
 
             # 2. Update heading (accumulate turn each step)
             theta += turn_rad_per_step
@@ -139,18 +137,18 @@ class WalkingRobot:
             #    Leg bases are then:  T_wb * leg_offset_in_body_frame  (Eq. 7.3)
             T_wb = sm.SE3(pos_x, pos_y, 0) * sm.SE3.Rz(theta)
 
-            for j, leg_robot in enumerate(legs):
-                leg_robot.base = T_wb * leg_offsets[j]
+            for j, leg_robot in enumerate(self.legs):
+                leg_robot.base = T_wb * self.leg_offsets[j]
 
             body.base = T_wb
             ax = env.fig.axes[0]
             cam_dist = 0.4
-            if anim_skip_every <= 0:
+            if self._skip <= 0:
                 env.step(dt=dt_anim)
                 env.step(dt=dt_anim)
                 ax.set_xlim(pos_x - cam_dist, pos_x + cam_dist)
                 ax.set_ylim(pos_y - cam_dist, pos_y + cam_dist)
-            elif i % anim_skip_every == 0:
+            elif i % self._skip == 0:
                 env.step(dt=dt_anim)
                 ax.set_xlim(pos_x - cam_dist, pos_x + cam_dist)
                 ax.set_ylim(pos_y - cam_dist, pos_y + cam_dist)
@@ -160,9 +158,37 @@ class WalkingRobot:
         env.hold()
         plt.close('all')
 
+    def _create_leg(self) -> rt.ERobot:
+        print("creating the leg")
+        leg = rt.ERobot(rt.ET.Rz() * rt.ET.Rx() * rt.ET.ty(self.L1) * rt.ET.Rx() * rt.ET.tz(-self.L2))
+        print("Leg: ", leg)
+        return leg
+
+    def _create_legs(self, leg: rt.ERobot) -> list:
+        legs = [
+            rt.ERobot(leg, name='leg0'),
+            rt.ERobot(leg, name='leg1'),
+            rt.ERobot(leg, name='leg2'),
+            rt.ERobot(leg, name='leg3')
+        ]
+
+        return legs
+
+    def _create_leg_offsets(self) -> list:
+        leg_offsets = [
+            sm.SE3(self.BODY_WIDTH/2,  -self.BODY_WIDTH/2, 0),
+            sm.SE3(-self.BODY_LENGTH/2,  -self.BODY_WIDTH/2, 0),
+            sm.SE3(self.BODY_LENGTH/2,   self.BODY_WIDTH/2, 0) * sm.SE3.Rz(np.pi),
+            sm.SE3(-self.BODY_LENGTH/2,   self.BODY_WIDTH/2, 0) * sm.SE3.Rz(np.pi),
+        ]
+        return leg_offsets
+
     def _gait(self, cycle, k, offset, flip):
         k = (k + offset) % cycle.shape[0]
         q = cycle[k, :].copy()
         if flip:
             q[0] = -q[0]
         return q
+
+    def run(self):
+        pass
